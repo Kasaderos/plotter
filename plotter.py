f53f3fa129a6bb2d9b49dd38e0c8f454b47950d3
kasaderos <hakaze0000@gmail.com>
2019-05-19 16:21:03 +0300

    realized main function plot

--- plotter.py
+++ plotter.py
@@ -1,17 +1,49 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-
+from py_expression_eval import Parser
+'''
+sinonims = {["arctg", "arctag"] : "atan", 
+                  ["arcsin",] : "asin",
+                  ["arccos",] : "acos",
+                  ["ln", ] : "log",
+                  ["tg", ] : "tan",
+                  ["e", "E"] : "exp",
+                  ["pi", ] : "PI"}
+'''
 class Plotter():
     
-    def __init__(self):
-        self.functions = []
+    def __init__(self, functions = []):
+        self.parser = Parser()
+        self.expressions = self.set(functions) 
+        self.functions = functions
+
+    def get(self):
+        return self.expressions 
     
+    def set(self, functions):
+        self.functions = functions
+        self.expressions = [self.parser.parse(i) for i in functions]
+        
     def add(self, function):
-        self.functions.append(function)
+        # add function to Plotter
+        self.expressions.append(function)
 
-    def del(self, function):
-        self.functions.remove(function)
+    def delete(self, function):
+        # delete function from Plotter
+        self.expressions.remove(function)
 
+    def plot(self, function, **arg):
+        # plots the function
+        if function in self.functions:
+            print("starting draw")
+            x = np.linspace(0, 2, 100)              
+            y = [self.parser.parse(function).evaluate({'x':i}) for i in x]
+            plt.plot(x, y, label='function')
+            plt.show()
 
-
-
+plotter = Plotter()
+plotter.set(['x', 'x^2', 'x^3',])
+plotter.plot(['x', 'x^2'])
+plotter.plot('x^2')
+plotter.plot('x^3')
---
+++ tmp.py
@@ -0,0 +1,9 @@
+import matplotlib
+
+def f(a, b):
+    return a + b
+
+fs = []
+#parser = matplotlib.mathtext.Parser()
+
+exec("f(1,2)")
