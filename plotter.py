38409a5a884f030cd22a535e21ee88e0167f98dc
kasaderos <hakaze0000@gmail.com>
2019-05-21 15:21:58 +0300

    parser.py finished

--- plotter.py
+++ plotter.py
@@ -11,91 +11,64 @@ sinonims = {["arctg", "arctag"] : "atan",
                   ["e", "E"] : "exp",
                   ["pi", ] : "PI"}
 '''
-TNUMBER = 0
-TOP1 = 1
-TOP2 = 2
-TVAR = 3
-TFUNCALL = 4
-
-def check(expr, values = {}):
-    values = values or {}
-    nstack = []
-    L = len(expr.tokens)
-    for item in expr.tokens:
-        type_ = item.type_
-        if type_ == TNUMBER:
-            nstack.append(item.number_)
-        elif type_ == TOP2:
-            n2 = nstack.pop()
-            n1 = nstack.pop()
-            f = expr.ops2[item.index_]
-            nstack.append(f(n1, n2))
-        elif type_ == TVAR:
-            if item.index_ in values:
-                nstack.append(values[item.index_])
-            elif item.index_ in expr.functions:
-                nstack.append(expr.functions[item.index_])
-            else:
-                return False #raise Exception('undefined variable: ' + item.index_)
-        elif type_ == TOP1:
-            n1 = nstack.pop()
-            f = expr.ops1[item.index_]
-            nstack.append(f(n1))
-        elif type_ == TFUNCALL:
-            n1 = nstack.pop()
-            f = nstack.pop()
-            if callable(f):
-                if type(n1) is list:
-                    nstack.append(f(*n1))
-                else:
-                    nstack.append(f(n1))
-            else:
-                return False # Exception(f + ' is not a function')
-        else:
-            return False # raise Exception('invalid Expression')
-    if len(nstack) > 1:
-        return False #raise Exception('invalid Expression (parity)')
-    return True
-
+info = { "label": "function",
+          "data": False,
+         "color": "b",
+             "x": np.arange(-10, 10, 0.1),
+             "y": None,
+        "xlabel": 'x label',#None
+        "ylabel": 'y label',#None
+         "title": "Plotter result",
+        "legend": True
+        }
 
 class Plotter():
     
     def __init__(self):
         self.parser = Parser()
-        self.functions = []
-        self.expressions = []
+        self.funcs = {}
             
     def get(self):
-        return self.expressions
+        return self.funcs
 
-    def set(self, functions):
+    def set(self, funcs):
         # parsing
-        self.functions = functions
-        L = [self.parser.parse(i) for i in functions]
-        #self.expressions = [w for w in L if check(w)]
-        for w in L:
-            if check(w):
-                self.expressions.append(w)
-
-    def add(self, function):
-        # add function to Plotter
-        self.expressions.append(function)
-
-    def delete(self, function):
-        # delete function from Plotter
-        self.expressions.remove(function)
-
-    def plot(self, function, **arg):
-        # plots the function
-        if arg == {} and (function in self.functions):
-            print("starting draw " + function)
-            x = np.linspace(0, 2, 100)
-            y = [self.parser.parse(function).evaluate({'x':i}) for i in x]
-            plt.plot(x, y, label='function')
-            plt.show()
+        self.funcs = {k:self.parser.parse(k) for k in funcs}
         
+    def add(self, f):
+        # add function to Plotter
+        self.funcs[f] = self.parser.parse(f)
+
+    def delete(self, f):
+        # delete function from Plotter
+        self.funcs.pop(f)
+
+    def setInfo(self, **info):
+        if info == {}:
+            return
+        if not info["data"]:
+            info["y"] = np.array([self.funcs[f].evaluate({'x':i}) for i in info["x"]])
+        plt.xlabel(info["xlabel"])
+        plt.ylabel(info["ylabel"])
+        plt.title(info["title"])
+
+    def plot(self, f = [], **info):
+        # plot the function
+        self.setInfo(info = info)
+        if f == [] and self.funcs == {}:
+            print("nothing to plot")
+            return
+        elif f == []:
+            for func in self.funcs.keys():
+                info["y"] = np.array([self.func.evaluate({'x':i}) for i in info["x"]])
+                plt.plot(info["x"], info["y"], label=info["label"])        
+        elif len(f) == 1 and (f[0] in self.funcs):
+            plt.plot(info["x"], info["y"], label=info["label"])
+        plt.show()
 
 plotter = Plotter()
-plotter.set(['x', 'x^2', 'x^3',])
-plotter.plot('x^2')
-plotter.plot('x^3')
+try:
+    plotter.add('sin(x)')
+    plotter.plot(info=info)
+except Exception as e:
+    print(e)
--- tmp.py
+++ tmp.py
@@ -1,5 +1,53 @@
 import matplotlib
 
+TNUMBER = 0
+TOP1 = 1
+TOP2 = 2
+TVAR = 3
+TFUNCALL = 4
+
+def check(expr, values = {}):
+    values = values or {}
+    nstack = []
+    L = len(expr.tokens)
+    for item in expr.tokens:
+        type_ = item.type_
+        if type_ == TNUMBER:
+            nstack.append(item.number_)
+        elif type_ == TOP2:
+            n2 = nstack.pop()
+            n1 = nstack.pop()
+            f = expr.ops2[item.index_]
+            nstack.append(f(n1, n2))
+        elif type_ == TVAR:
+            if item.index_ in values:
+                nstack.append(values[item.index_])
+            elif item.index_ in expr.functions:
+                nstack.append(expr.functions[item.index_])
+            else:
+                return False # undefined variable:  item.index_
+        elif type_ == TOP1:
+            n1 = nstack.pop()
+            f = expr.ops1[item.index_]
+            nstack.append(f(n1))
+        elif type_ == TFUNCALL:
+            n1 = nstack.pop()
+            f = nstack.pop()
+            if callable(f):
+                if type(n1) is list:
+                    nstack.append(f(*n1))
+                else:
+                    nstack.append(f(n1))
+            else:
+                return False #f  is not a function
+        else:
+            return False # invalid Expression
+    if len(nstack) > 1:
+        return False # invalid Expression (parity)
+    return True
+
+
+
 def f(a, b):
     return a + b
 
