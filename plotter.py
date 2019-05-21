a68e7aeeb680e50718b74369ba31f99319a92c82
kasaderos <hakaze0000@gmail.com>
2019-05-21 23:45:34 +0300

    parser.check(f) fixed

--- plotter.py
+++ plotter.py
@@ -21,24 +21,24 @@ class Plotter():
         self.info = { "label": "function",
                       "data": False,
                      "color": "b",
-                         "x": np.linspace(-5.0, 5.0, 151),
+                         "x": np.linspace(-1.0, 1.0, 51),
                          "y": None,
                     "yRange": [-3.0, 3.0],
-                    "xLabel": 'x label',#None
-                    "yLabel": 'y label',#None
+                    "xLabel": 'x label',
+                    "yLabel": 'y label',
                "xyLabelTrue": False,
                      "title": "Plotter result",
                     "legend": True
                 }
         
-    def get(self):
-        return self.functions
-
     def add(self, f, **exInfo):
         # add function to Plotter
         expression = self.parser.check(f)
         if expression != None:
             self.functions[f] = expression
+        else:
+            print("parser: can't add the function " + f)
+            return
         if exInfo:
             self.fInfo[f] = exInfo
         else:
@@ -52,19 +52,24 @@ class Plotter():
 
     def setY(self, f, x):
         y = [self.functions[f].evaluate({x:i}) for i in self.fInfo[f]['x']]
-        self.fInfo[f]['y'] = np.array(y)
-
+        self.fInfo[f]['y'] = np.array(y) 
+            
     def plotF(self, f): 
         d = self.functions[f].symbols()
         if len(d) != 1:
             raise Exception('too much arguments of function')
+        elif d[0] == f:
+            raise Exception("plotF: error input, the function " + f)
         F = self.fInfo[f]
         if F["xyLabelTrue"]:
             plt.xlabel(F["xlabel"])
             plt.ylabel(F["ylabel"])
         plt.title(F["title"])
-        self.setY(f, d[0])
-        plt.plot(F['x'], F['y'], label=F['label'])
+        try:
+            self.setY(f, d[0])
+            plt.plot(F['x'], F['y'], label=F['label'])
+        except Exception as e:
+            print("evaluate: " + f + " doesn't evaluate")
 
     def plot(self, f = None, legend = False):
         # plot the function
@@ -78,15 +83,19 @@ class Plotter():
         plt.show()
 
 plotter = Plotter()
-plotter.add('1/t')
-plotter.add('1/(1-t)')
-plotter.add('sin(x)')
-#try:
-#plotter.plot('1/t')
-    #plotter.plot('1/(1-t)')
-    #plotter.plot('sin(x)')
-plotter.plot(legend = True)
-    #print(plotter.get())
-#except Exception as e:
- #   print(e)
+plotter.add('abs(x)')
+plotter.add('exp(x)')
+try:
+    plotter.plot(legend = True)
+    #plotter.plot('logx')
+    #plotter.plot('logx-')
+    #plotter.plot('abs(x)')
+    #plotter.plot('exp(x)')
+except Exception as e:
+    print(e)
 
+'''
+sin(1/(1+x^2))
+exp(-x^2)
+ln(1/(1+x^2))
+'''
